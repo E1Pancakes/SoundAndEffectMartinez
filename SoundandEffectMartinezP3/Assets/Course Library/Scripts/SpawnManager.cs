d42b34d83edcb7707c1cc7e7a70f39aa00827878
Diego Martinez <718717@student.dusd.net>
2024-04-10 11:38:11 -0700

    i added replacing Background

---
+++ SoundandEffectMartinezP3/Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstticalPrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void SpawnObstacle ()
+    {
+        Instantiate(obstticalPrefab, spawnPos, obstticalPrefab.transform.rotation);
+    }
+}
